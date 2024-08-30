ccd25cd02677d6c56e4b15f784adcebec1cb87ae
saisravya15 <saisravyaavula1@gmail.com>
2024-08-30 09:04:19 +0530

    fragment project

---
+++ 12--fragment/app/src/main/java/com/example/a12__fragment/YahooFragment.java
@@ -0,0 +1,20 @@
+package com.example.a12__fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class YahooFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {V
+    }
+}
